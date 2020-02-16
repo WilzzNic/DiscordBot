3200f441b7ffb567c3866c79078a704f1f7f1bcb
Wilson Nicholas <wilzznic05@gmail.com>
2020-02-16 11:33:21 +0700

    Problem with displaying pagination

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const Pagination = require('discord-paginationembed');
+const axios = require('axios').default;
 
 require('dotenv').config();
 
@@ -8,10 +10,43 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+var FieldsEmbed = null;
+
 client.on('message', message => {
-    if(message.content === '!ping') {
-        message.channel.send('Pong.');
+    if(message.author.bot) return;
+
+    if(message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ !/g);
+    const command = args.shift().toLowerCase();
+
+    if(command == "countries") {
+        axios.get('https://holidayapi.com/v1/countries?key=e5428f2a-2fab-4b47-9269-d49bb36d35d3')
+        .then(function (response) {
+            console.table(response);
+            var object = response;
+
+            FieldsEmbed = new Pagination.FieldsEmbed()
+                .setArray(object.countries)
+                .setAuthorizedUsers([])
+                .setChannel(message.channel)
+                .setElementsPerPage(10)
+                .setPageIndicator(true)
+                .formatField('Continue..', o => o.name);
+            
+            FieldsEmbed.build();
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+            
+        });
     }
+
+    // message.channel.send(countries); 
+    
 });
 
 client.login(process.env.DISCORD_TOKEN);
